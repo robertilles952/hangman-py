d1b6219f739a3a066dec439d47d013df4245d223
Robert Illes <robee@DESKTOP-S0P9NBT.localdomain>
2026-09-26 15:52:23 +0200

    implement task 5

--- hangman.py
+++ hangman.py
@@ -4,20 +4,26 @@ print("Welcome to Hangman!")
 
 words = ["kecske", "alma", "laptop", "telefon", "automobil"]
 word = random.choice(words)
-given_letter = input("give me a letter:")
-guesses = [given_letter]
+guesses = []
 
-def show_hidden_word(secret_word, guessed_letters):
-    word_i_need_to_guess = " "
-    for letter in secret_word:
-        if letter in guessed_letters:
-            word_i_need_to_guess = word_i_need_to_guess + letter
-            print("Good guess!")
-        else:
-            word_i_need_to_guess = word_i_need_to_guess + "_"
-            print("Not in the word!")
+while True: 
+    given_letter = input("give me a letter:")
+    if given_letter in guesses:
+        continue
+    else:
+        guesses.append(given_letter)
+    def show_hidden_word(secret_word, guessed_letters):
+        word_i_need_to_guess = " "
+        for letter in secret_word:
+            if letter in guessed_letters:
+                word_i_need_to_guess = word_i_need_to_guess + letter
+                print("Good guess!")
+            else:
+                word_i_need_to_guess = word_i_need_to_guess + "_"
+                print("Not in the word!")
 
-    return word_i_need_to_guess
+        return word_i_need_to_guess
 
-hidden_word = show_hidden_word(word, guesses)
-print(hidden_word)
+    hidden_word = show_hidden_word(word, guesses)
+    print(hidden_word)
+
