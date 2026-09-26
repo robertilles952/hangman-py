baf2af8885499bc53e0458dc2677af46ff02053f
Robert Illes <robee@DESKTOP-S0P9NBT.localdomain>
2026-09-26 18:14:30 +0200

    implement task 6

--- hangman.py
+++ hangman.py
@@ -14,26 +14,30 @@ while True:
         continue
     else:
         guesses.append(given_letter)
-    def show_hidden_word(secret_word, guessed_letters, lives):
+    def show_hidden_word(secret_word, guessed_letters):
         word_i_need_to_guess = " "
-        is_good_guess = False
         
         for letter in secret_word:
             if letter in guessed_letters:
-                word_i_need_to_guess = word_i_need_to_guess + letter
-                is_good_guess = True
-                print("Good guess!")
+                word_i_need_to_guess = word_i_need_to_guess + letter                
             else:
                 word_i_need_to_guess = word_i_need_to_guess + "_"
-                print("Not in the word!")
-        if is_good_guess == False:
-                    lives -= 1
-        return word_i_need_to_guess, is_good_guess, lives
+                
+        return word_i_need_to_guess
     
+    def count_lives(lives, given_letter, hiding_one):
+        if given_letter in hiding_one:
+            print("Good guess!")
+        else:
+            lives -= 1
+            print("Not in the word!")
+        #print("Remaining lives : " + str(lives))
+        #print("Hello from count lives:" + str(lives), str(given_letter), hiding_one)
+        return lives
     
-    hidden_word = show_hidden_word(word, guesses, lives)
-    lives = hidden_word[2]
-    print(hidden_word[2])
-    print(hidden_word[0])
-    print(hidden_word[1])
+    hidden_word = show_hidden_word(word, guesses)
+    how_much_life = count_lives(lives, given_letter, hidden_word)
+    lives = how_much_life
+    print("Remaining lives : " + str(how_much_life))
+    print(hidden_word)
 
